8aaeea32ab5e565846732f5a5c344f38de8099d0
biancacosta12 <67914457+biancacosta12@users.noreply.github.com>
2020-07-09 11:22:58 -0300

    Create 1012.area.c

---
+++ 1012.area.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+    double A,B,C; //pontos
+    scanf("%lf %lf %lf",&A,&B,&C);
+    double triangulo= (A*C)/2.0;
+    double circulo = 3.14159*C*C;
+    double trapezio = (A+B)/2.0*C;
+    double quadrado= B*B;
+    double retangulo = A*B;
+    printf("TRIANGULO: %.3lf\n",triangulo);
+    printf("CIRCULO: %.3lf\n",circulo);
+    printf("TRAPEZIO: %.3lf\n",trapezio);
+    printf("QUADRADO: %.3lf\n",quadrado);
+    printf("RETANGULO: %.3lf\n", retangulo);
+    return 0;
+}
