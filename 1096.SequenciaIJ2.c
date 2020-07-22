7b9503fffcaf3303dffd758ee3b673393a0fbc41
biancacosta12 <67914457+biancacosta12@users.noreply.github.com>
2020-07-22 12:19:28 -0300

    Create 1096.SequenciaIJ2.c

---
+++ 1096.SequenciaIJ2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+    int i=1,j=7;
+    while(i<=9){
+        int k=0;
+        for(k=0;k<3;k++){
+            printf("I=%d J=%d\n",i,j);
+            j=j-1;
+        }
+        i=i+2;
+        j=j+3;
+    }
+    return 0;
+}
