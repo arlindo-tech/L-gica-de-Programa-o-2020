86d5c6df796d91a5b70749161bd248476e8ff7ed
biancacosta12 <67914457+biancacosta12@users.noreply.github.com>
2020-08-23 19:59:23 -0300

    Create 1168.LED.c

---
+++ 1168.LED.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int N; // número de testes
+    scanf("%d",&N);
+    int k=0;
+    while(k!=N){
+        char numero[1000];
+        scanf("%s", numero);
+        int leds=0;// quantos leds serão necessários
+        int i;
+        int l= strlen(numero);
+        for(i=0;i<l;i++){
+            if(numero[i]=='1'){
+                leds= leds+2; // numero 1 precisa de 2leds
+            }else if(numero[i]=='2'|| numero[i]=='3' || numero[i]=='5'){
+                leds= leds+5; // precisam de 5 leds
+            }else if(numero[i]=='4'){
+                leds= leds+4;
+            }else if(numero[i]=='6' || numero[i]=='9'|| numero[i]=='0'){
+                leds=leds+6;
+            }else if(numero[i]=='7'){
+                leds= leds+3;
+            }else if(numero[i]=='8'){
+                leds=leds+7;
+            }
+        }
+        printf("%d leds\n", leds);
+        leds=0;
+        k++;
+    }    
+    return 0;
+}
