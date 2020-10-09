eefb9f5092492b61ecb4388a84fc0e8d5056b534
arlindo-tech <72632840+arlindo-tech@users.noreply.github.com>
2020-10-09 19:46:03 -0300

    Update 1383.sudoku.c
    Só ajustes estéticos. OK!? Coloquei mais espaços para melhorar a legibilidade.
    Boas férias! Excelente desempenho na disciplina.
    
    Quando voltar as aulas eu pago o chocolate que eu estou devendo. Se eu esquecer me, por favor avise :-)

--- 1383.sudoku.c
+++ 1383.sudoku.c
@@ -9,6 +9,7 @@ int main() {
     int i,j,p; // usadas nos for loops
     int linhas=0,colunas=0, matrizes=0;
     int SimOuNao=0; // SimOuNão vai ser 0=sim e 1=não
+    
     // usando o while para ler as n matrizes, conforme a entrada
     while(k!=n){
         k++; // para dar o valor corrento na instância
@@ -26,8 +27,9 @@ int main() {
                 }
             }
         }
-// matriz pronta, vamos fazer a verificação se funcionou
-// testando linha e coluna, se tem elementos iguais, se tiver não será solução
+        
+        // matriz pronta, vamos fazer a verificação se funcionou
+        // testando linha e coluna, se tem elementos iguais, se tiver não será solução
         for(p=0;p<9;p++){
             for(i=0;i<9;i++){
                 for(j=0;j<9;j++){
@@ -36,23 +38,26 @@ int main() {
                             linhas=1;// ou seja, não é solução
                         }
                         if(sudoku[i][p]==sudoku[j][p]){
-                        colunas=1; // ou seja não é solução
+                            colunas=1; // ou seja não é solução
                         }
                     }
                  }
             }
         }
-// por fim, só precisa testar as matrizes 3x3
+
+        // por fim, só precisa testar as matrizes 3x3
         for(i=0;i<=6;i+=3){
             for(j=0;j<=6;j+=3){
                 matrizes= matrizes3x3(&sudoku[i][j]);
             }
         }
-// agora que as linhas e colunas foram testadas: verificar o SIM OU Não
+        
+        // agora que as linhas e colunas foram testadas: verificar o SIM OU Não
         if(colunas==1 || linhas==1 || matrizes==1){
             SimOuNao=1; // não é uma solução válida
         }
         printf("Instancia %d\n",k);
+        
         // agora que já está verificado, IMPRIMIR SIM OU NAO
         if(SimOuNao){
             printf("NAO\n");
@@ -75,8 +80,8 @@ int matrizes3x3(int* matriz){
     // neste caso: a primeira linha só precisa somar: 0, 1 e 2
     // segundo linha: 9+0; 9+1; 9+2 (9 pois é a distância do primeiro elemento da primeira linha até o primeiro da segunda)
     // terceira linha: 18+0, 18+1, 18+2; (18 pois é a distância do 1º elemento da 1ª linha para o 1º elemento da 2ª linha)
-    int teste[]={*matriz, *(matriz+1),*(matriz+2), // primeira linha
-                    *(matriz+9),*(matriz+10), *(matriz+11), // segunda linha
+    int teste[]={*matriz, *(matriz+1), *(matriz+2), // primeira linha
+                    *(matriz+9), *(matriz+10), *(matriz+11), // segunda linha
                     *(matriz+18), *(matriz+19), *(matriz+20)}; // terceira linha
     // só falta testar se alguns desses elementos é repetido                
     for(i=0;i<9;i++){
